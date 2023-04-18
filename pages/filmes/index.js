45e9864ed3308ad5d2f8ade5e212655b910f2417
Guilherme Vaz <guivs56@gmail.com>
2023-04-17 22:16:10 -0300

    novo 17/04

--- pages/filmes/index.js
+++ pages/filmes/index.js
@@ -2,27 +2,38 @@ import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pagina from '@/components/Pagina';
 import apifilmes from '@/services/apiFilmes';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+import Link from 'next/link';
 
 const index = (props) => {
 
   return (
-    
+
     <Pagina titulo="Filmes">
-      {props.filmes.map(item=>(
-        <p>{item.title}</p>
-        
-      
-      
-      
-    ))}
-       
-      
-      
-        
-  
+
+      <Row md={4}>
+
+        {props.filmes.map(item => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src= {"https://image.tmdb.org/t/p/w500" + item.backdrop_path} />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <p>Lançamento: {item.release_date}</p>
+                <p>Nota: {item.vote_average}</p>
+                <Link className='btn btn-danger' href={'/filmes/' + item.id}>Detalhes</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+
+      </Row>
+
+
+
 
     </Pagina>
-    
+
   )
 }
 
@@ -33,6 +44,6 @@ export async function getServerSideProps(context) {
   const filmes = resultado.data.results
 
   return {
-    props: {filmes},
+    props: { filmes },
   }
 }
